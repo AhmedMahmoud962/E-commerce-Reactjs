3095e2ef162e4b8a4fe3f065f10d3bd29e86550d
Ahmed Mahmoud <103325024+AhmedMahmoud962@users.noreply.github.com>
2023-09-04 00:22:18 +0300

    Update Card.js

--- src/components/Products/Card.js
+++ src/components/Products/Card.js
@@ -11,7 +11,7 @@ const Card = ({ image, title, price }) => {
       data-aos-easing="ease-out-cubic"
       data-aos-duration="2000"
     >
-      <img src={image} alt={title} className="card-image" />
+      <img loading="lazy" src={image} alt={title} className="card-image" />
 
       <p className="card-title">Product {title}</p>
 
@@ -25,7 +25,7 @@ const Card = ({ image, title, price }) => {
         <h3 className="card-title" style={{ opacity: ".5" }}>
           {price}
         </h3>
-        <img
+        <img 
           className="card-title"
           style={{ width: "25px" }}
           src="images/icon.png"
